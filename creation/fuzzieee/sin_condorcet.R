0697e44097445ca8e5e25a2ea379f4ff56e4186d
Noelia <noeliarico@uniovi.es>
2021-02-15 16:00:55 +0100

    New profiles

--- creation/fuzzieee/sin_condorcet.R
+++ creation/fuzzieee/sin_condorcet.R
@@ -1,35 +1,74 @@
-path <- file.path("creation","scripts")
-# n = 5 -------------------------------------------------------------------
+pors4cr <- create_profiles_condorcet(4,10,5,max_iter = 100000)
+pors5cr <- create_profiles_condorcet(5,10,5,max_iter = 100000)
+pors6cr <- create_profiles_condorcet(6,10,5,max_iter = 100000)
+pors7cr <- create_profiles_condorcet(7,10,5,max_iter = 100000)
+pors8cr <- create_profiles_condorcet(8,10,5,max_iter = 100000) 
+pors9cr <- create_profiles_condorcet(9,10,5,max_iter = 1000000) 
+pors10cr <- create_profiles_condorcet(10,10,5,max_iter = 100000)
+pors11cr <- create_profiles_condorcet(11,10,5,max_iter = 100000)
+pors12cr <- create_profiles_condorcet(12,10,5,max_iter = 100000)
+pors13cr <- create_profiles_condorcet(13,10,5,max_iter = 100000)
+pors14cr <- create_profiles_condorcet(14,10,5,max_iter = 100000)
 
-pors4 <- create_profiles(4,10,5)
-pors5 <- create_profiles(5,10,5)
-pors6 <- create_profiles(6,10,5)
-pors7 <- create_profiles(7,10,5,max_iter = 1000)
-pors8 <- create_profiles(8,10,5,max_iter = 100000) 
-pors9 <- create_profiles(9,10,5,max_iter = 1000000) # 6143
-pors10 <- create_profiles(10,10,5,max_iter = 100000)
-pors11 <- create_profiles(11,10,5,max_iter = 100000)
-pors12 <- create_profiles(12,10,5,max_iter = 100000)
-pors13 <- create_profiles(13,10,5,max_iter = 100000)
-pors14 <- create_profiles(14,10,5,max_iter = 100000)
+save(pors4cr,
+     pors5cr,
+     pors6cr,
+     pors7cr,
+     pors8cr,
+     pors9cr,
+     pors10cr,
+     pors11cr,
+     pors12cr,
+     pors13cr,
+     pors14cr,
+     file = "creation/fuzzieeeCR.RData")
 
+pors4cw <- create_profiles_condorcet_winner(4,10,5,max_iter = 100000)
+pors5cw <- create_profiles_condorcet_winner(5,10,5,max_iter = 100000)
+pors6cw <- create_profiles_condorcet_winner(6,10,5,max_iter = 100000)
+pors7cw <- create_profiles_condorcet_winner(7,10,5,max_iter = 100000)
+pors8cw <- create_profiles_condorcet_winner(8,10,5,max_iter = 100000) 
+pors9cw <- create_profiles_condorcet_winner(9,10,5,max_iter = 1000000) 
+pors10cw <- create_profiles_condorcet_winner(10,10,5,max_iter = 100000)
+pors11cw <- create_profiles_condorcet_winner(11,10,5,max_iter = 100000)
+pors12cw <- create_profiles_condorcet_winner(12,10,5,max_iter = 100000)
+pors13cw <- create_profiles_condorcet_winner(13,10,5,max_iter = 100000)
+pors14cw <- create_profiles_condorcet_winner(14,10,5,max_iter = 100000)
 
-save(pors4,
-     pors5,
-     pors5,
-     pors6,
-     pors7,
-     pors8,
-     pors9,
-     pors5,
-     pors10,
-     pors11,
-     pors12,
-     pors13,
-     pors14,
-     file = "creation/fuzzieee.RData")
+save(pors4cw,
+     pors5cw,
+     pors6cw,
+     pors7cw,
+     pors8cw,
+     pors9cw,
+     pors10cw,
+     pors11cw,
+     pors12cw,
+     pors13cw,
+     pors14cw,
+     file = "creation/fuzzieeeCW.RData")
 
-write <- lapply(pors, function(x) lapply(x, function(y) to_python_om(votrix(y))))
-sink(file.path(path, "n5.py"))
-cat(paste(unlist(write), collapse = "\n"))
-sink()
+pors4nc <- create_profiles_no_condorcet(4,10,5,max_iter = 100000)
+pors5nc <- create_profiles_no_condorcet(5,10,5,max_iter = 100000)
+pors6nc <- create_profiles_no_condorcet(6,10,5,max_iter = 100000)
+pors7nc <- create_profiles_no_condorcet(7,10,5,max_iter = 100000)
+pors8nc <- create_profiles_no_condorcet(8,10,5,max_iter = 100000) 
+pors9nc <- create_profiles_no_condorcet(9,10,5,max_iter = 1000000) 
+pors10nc <- create_profiles_no_condorcet(10,10,5,max_iter = 100000)
+pors11nc <- create_profiles_no_condorcet(11,10,5,max_iter = 100000)
+pors12nc <- create_profiles_no_condorcet(12,10,5,max_iter = 100000)
+pors13nc <- create_profiles_no_condorcet(13,10,5,max_iter = 100000)
+pors14nc <- create_profiles_no_condorcet(14,10,5,max_iter = 100000)
+
+save(pors4nc,
+     pors5nc,
+     pors6nc,
+     pors7nc,
+     pors8nc,
+     pors9nc,
+     pors10nc,
+     pors11nc,
+     pors12nc,
+     pors13nc,
+     pors14nc,
+     file = "creation/fuzzieeeNC.RData")
--- creation/porssincuartiles.R
+++ creation/porssincuartiles.R
@@ -6,7 +6,7 @@ n <- 8 # number of alternatives
 nvotes <- 10
 reps <- 2 # number of profiles of rankings with each characteristic
 
-create_profiles <- function(n, nvotes, reps, max_iter = Inf)  {
+create_profiles_condorcet <- function(n, nvotes, reps, max_iter = Inf)  {
   
   # Create the list that contains all the profile of rankings
   # It is n-1 because for the case when the n alternatives have rowsum>=colsum
@@ -36,7 +36,113 @@ create_profiles <- function(n, nvotes, reps, max_iter = Inf)  {
       v <- votrix(r)
       w <- sum(rowSums(v) >= colSums(v))
       
-      if(w != n && !condorcet(r)) { 
+      if(w != n && any(condorcet(r))) { 
+        
+        # Agreement among the voters of the profile of rankings
+        ag <- agreement_margin(v)
+        # Get the index to insert the profile of rankings in
+        i <- get_available_index(profiles[[w]])
+        
+        if(!is.null(i)) {
+          print(paste("w =", w, "-- i =", i))
+          profiles[[w]][[i]] <- r
+          created <- created + 1
+        }
+      }
+    }
+    
+    seed <- seed + 1
+    
+  }
+  
+  return(profiles)
+  
+}
+
+create_profiles_condorcet_winner <- function(n, nvotes, reps, max_iter = Inf)  {
+  
+  # Create the list that contains all the profile of rankings
+  # It is n-1 because for the case when the n alternatives have rowsum>=colsum
+  # the uncertainty is max
+  profiles <- vector(mode = "list", length = (n-1))
+  names(profiles) <- paste0("w",1:(n-1))
+  # Profiles of rankings
+  pr <- vector(mode = "list", length = reps)
+  names(pr) <- paste0("pr",1:reps)
+  # Empty spaces for each repetition
+  profiles <- lapply(profiles, function(x) x <- pr)
+  
+  seed <- 1
+  created <- 0
+  #while(created < ((n-1)*reps) && seed < 500) {
+  
+  #if(is.null(max_iter)) cond <- created < ((n-1)*reps)
+  #else cond <- (created < ((n-1)*reps) && seed < max_iter)
+  
+  while(created < ((n-1)*reps) && seed < max_iter) {
+    
+    print(paste(seed,"#################################################"))
+    
+    for(d in 2:nvotes) {
+      
+      r <- random_profile_of_rankings(n, nvotes, seed = seed, distinct = d)
+      v <- votrix(r)
+      w <- sum(rowSums(v) >= colSums(v))
+      
+      if(w != n && !condorcet(r) && condorcet_winner(r)) { 
+        
+        # Agreement among the voters of the profile of rankings
+        ag <- agreement_margin(v)
+        # Get the index to insert the profile of rankings in
+        i <- get_available_index(profiles[[w]])
+        
+        if(!is.null(i)) {
+          print(paste("w =", w, "-- i =", i))
+          profiles[[w]][[i]] <- r
+          created <- created + 1
+        }
+      }
+    }
+    
+    seed <- seed + 1
+    
+  }
+  
+  return(profiles)
+  
+}
+
+create_profiles_no_condorcet <- function(n, nvotes, reps, max_iter = Inf)  {
+  
+  # Create the list that contains all the profile of rankings
+  # It is n-1 because for the case when the n alternatives have rowsum>=colsum
+  # the uncertainty is max
+  profiles <- vector(mode = "list", length = (n-1))
+  names(profiles) <- paste0("w",1:(n-1))
+  # Profiles of rankings
+  pr <- vector(mode = "list", length = reps)
+  names(pr) <- paste0("pr",1:reps)
+  # Empty spaces for each repetition
+  profiles <- lapply(profiles, function(x) x <- pr)
+  
+  seed <- 1
+  created <- 0
+  #while(created < ((n-1)*reps) && seed < 500) {
+  
+  #if(is.null(max_iter)) cond <- created < ((n-1)*reps)
+  #else cond <- (created < ((n-1)*reps) && seed < max_iter)
+  
+  while(created < ((n-1)*reps) && seed < max_iter) {
+    
+    print(paste(seed,"#################################################"))
+    
+    for(d in 2:nvotes) {
+      
+      r <- random_profile_of_rankings(n, nvotes, seed = seed, distinct = d)
+      v <- votrix(r)
+      w <- sum(rowSums(v) >= colSums(v))
+      
+      if(w != n && !condorcet(r) && !condorcet_winner(r)) { 
         
         # Agreement among the voters of the profile of rankings
         ag <- agreement_margin(v)
